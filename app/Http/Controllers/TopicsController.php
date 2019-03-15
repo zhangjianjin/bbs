e1d2a37480bc32c53a69b965ea37d64e35b47386
Jianjin Zhang <583247597@qq.com>
2019-03-15 17:51:45 +0800

    用户可以编辑话题¼

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,27 +42,29 @@ class TopicsController extends Controller
 
         return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
     }
-	public function edit(Topic $topic)
-	{
+
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
+        $categories = Category::all();
+	return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    public function update(TopicRequest $request, Topic $topic)
+    {
+	$this->authorize('update', $topic);
+	$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+	return redirect()->route('topics.show', $topic->id)->with('message', '更新成功！');
+    }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic)
+    {
+	$this->authorize('destroy', $topic);
+	$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+	return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
