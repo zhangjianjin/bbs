2f426a1700ed1960f75244133c2607a34932e4c6
Jianjin Zhang <583247597@qq.com>
2019-03-15 18:15:00 +0800

    翻译 URL 标示

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -22,7 +23,11 @@ class TopicObserver
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
-
+        // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+ 
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
