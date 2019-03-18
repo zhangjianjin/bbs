d35fcbea02efe6808d39dc5f72ad0d902e0338fb
Jianjin Zhang <583247597@qq.com>
2019-03-18 11:03:10 +0800

    删除评论

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -24,12 +24,12 @@ class RepliesController extends Controller
 
         return redirect()->to($reply->topic->link())->with('success', '评论创建成功！');
     }
-
+    
     public function destroy(Reply $reply)
     {
         $this->authorize('destroy', $reply);
         $reply->delete();
 
-        return redirect()->route('replies.index')->with('success', '评论删除成功！');
+        return redirect()->to($reply->topic->link())->with('success', '评论删除成功！');
     }
 }
